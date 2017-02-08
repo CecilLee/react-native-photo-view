f8e7b92be5f2cc0e737eabf952f24411fd23f552
cecillee <cciscecil@gmail.com>
2017-02-08 22:15:42 +0800

    fixbug: 将anroid的实现改成ios的

--- PhotoView.android.js
+++ PhotoView.android.js
@@ -1,72 +1,32 @@
-import React, {Component, PropTypes} from 'react';
-import {requireNativeComponent, View} from 'react-native';
-
-const resolveAssetSource = require('react-native/Libraries/Image/resolveAssetSource');
+import React, {Component} from 'react';
+import {
+    View,
+    ScrollView,
+    TouchableWithoutFeedback
+} from 'react-native';
+import ProgressCircle from 'react-native-progress/Circle';
+import Image from 'react-native-image-progress';
 
 export default class PhotoView extends Component {
-    static propTypes = {
-        source: PropTypes.oneOfType([
-            PropTypes.shape({
-                uri: PropTypes.string
-            }),
-            // Opaque type returned by require('./image.jpg')
-            PropTypes.number
-        ]),
-        loadingIndicatorSource: PropTypes.oneOfType([
-            PropTypes.shape({
-                uri: PropTypes.string
-            }),
-            // Opaque type returned by require('./image.jpg')
-            PropTypes.number
-        ]),
-        fadeDuration: PropTypes.number,
-        minimumZoomScale: PropTypes.number,
-        maximumZoomScale: PropTypes.number,
-        scale: PropTypes.number,
-        androidZoomTransitionDuration: PropTypes.number,
-        androidScaleType: PropTypes.oneOf(["center", "centerCrop", "centerInside", "fitCenter", "fitStart", "fitEnd", "fitXY", "matrix"]),
-        onLoadStart: PropTypes.func,
-        onLoad: PropTypes.func,
-        onLoadEnd: PropTypes.func,
-        onTap: PropTypes.func,
-        onViewTap: PropTypes.func,
-        onScale: PropTypes.func,
-        ...View.propTypes
-    };
-
     render() {
-        const source = resolveAssetSource(this.props.source);
-        var loadingIndicatorSource = resolveAssetSource(this.props.loadingIndicatorSource);
+        return (
+            <ScrollView
+                contentContainerStyle={{ alignItems:'center', justifyContent:'center' }}
+                centerContent={true}
+                maximumZoomScale={this.props.maximumZoomScale}
+                minimumZoomScale={this.props.minimumZoomScale} 
+                showsHorizontalScrollIndicator={this.props.showsHorizontalScrollIndicator} 
+                showsVerticalScrollIndicator={this.props.showsVerticalScrollIndicator} 
+                >
 
-        if (source && source.uri === '') {
-            console.warn('source.uri should not be an empty string');
-        }
+                <TouchableWithoutFeedback
+                    onPress={this.props.onTap ? this.props.onTap : function() {}}>
 
-        if (this.props.src) {
-            console.warn('The <PhotoView> component requires a `source` property rather than `src`.');
-        }
+                    <Image indicator={ProgressCircle} indicatorProps={{color: 'gray'}} {...this.props}/>
 
-        if (source && source.uri) {
-            var {onLoadStart, onLoad, onLoadEnd} = this.props;
+                </TouchableWithoutFeedback>
 
-            var nativeProps = {
-                ...this.props,
-                shouldNotifyLoadEvents: !!(onLoadStart || onLoad || onLoadEnd),
-                src: source.uri,
-                loadingIndicatorSrc: loadingIndicatorSource ? loadingIndicatorSource.uri : null,
-            };
-
-            return <PhotoViewAndroid {...nativeProps} />
-        }
-        return null
+            </ScrollView>
+        );
     }
 }
-
-var cfg = {
-    nativeOnly: {
-        src: true,
-        loadingIndicatorSrc: true,
-        shouldNotifyLoadEvents: true
-    }
-};
-const PhotoViewAndroid = requireNativeComponent('PhotoViewAndroid', PhotoView, cfg);
